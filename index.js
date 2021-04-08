ee2118fee6fa777d47e56682e59fc8195a606eac
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-04-08 17:48:30 -0500

    wrapped inquirer prompt function in init()

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
-const renderLicenseBadge = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -109,15 +108,7 @@ const questions = [
       },
 ];
 
-inquirer.prompt(questions)
-    .then(data => {
-        console.log(data.license)
-        return generateMarkdown(data);
-    })
-    .then(markdownData => {
-        console.log(markdownData)
-        //return writeToFile(markdownData)
-    });
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -137,11 +128,18 @@ function writeToFile(fileName, data) {
 };
 
 // TODO: Create a function to initialize app
-// function init() {
-//     inquirer.prompt(questions);
-//     //.then(generateMarkdown and writeToFile
+function init() {
+    inquirer.prompt(questions)
+        .then(data => {
+            console.log(data.license)
+            return generateMarkdown(data);
+        })
+        .then(markdownData => {
+            console.log(markdownData)
+            //return writeToFile(markdownData)
+        });
     
-// }
+}
 
 // Function call to initialize app
-//init();
+init();
